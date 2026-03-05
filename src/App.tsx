cb0b9e49694f029aca8079c5370b46969f08c74f
wb_chenao <wb_chenao@kuaishou.com>
2026-03-05 16:22:08 +0800

    feat: 初始化项目结构与配置

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Models from './components/layout/index.tsx'
+
+function App() {
+  return (
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <Models />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/layout/index.tsx
@@ -0,0 +1,17 @@
+import { Canvas } from '@react-three/fiber'
+import './styles.css'
+import Box from './box.tsx'
+// import { Button, DatePicker } from 'antd'
+const Page = () => {
+  return (
+      <Canvas>
+        <ambientLight intensity={Math.PI / 2} />
+        <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
+        <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+        <Box position={[-1.2, 0, 0]} />
+        <Box position={[1.2, 0, 0]} />
+    </Canvas>
+  )
+}
+
+export default Page
